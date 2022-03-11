8112d2d68c0f03f17a437bbca71cfecadd6c867b
ravi <samcollins9899@gmail.com>
2022-03-11 19:43:30 +0530

    second commit

--- shared.js
+++ shared.js
@@ -4,22 +4,54 @@ var backdrop = document.querySelector('.backdrop')
 var model = document.querySelector('.modal')
 var modalNoButton = document.querySelector('.modal__action--negative')
 var selectPlanButton = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
 
+var mobileNav = document.querySelector('.mobile-nav');
+
+
+console.dir(backdrop )
 for( var i=0; i<selectPlanButton.length; i++)
 {
     selectPlanButton[i].addEventListener('click', function()
     {
-       model.style.display = 'block';
-       backdrop.style.display = 'block';
+    //    model.style.display = 'block';
+    //    backdrop.style.display = 'block';
+
+    model.classList.add('open')
+    backdrop.classList.add('open')
     })
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function (){
+    // mobileNav.style.display ='none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
 
-modalNoButton.addEventListener("click", closeModal);
+if(modalNoButton)
+{
+    modalNoButton.addEventListener("click", closeModal);
+}
+
+
 
 function closeModal()
 {
-    backdrop.style.display= 'none';
-    model.style.display ='none';
+    // backdrop.style.display= 'none';
+    // model.style.display ='none';
+    if(model)
+    {
+        model.classList.remove('open');
+       
+    }
+    backdrop.classList.remove('open'); 
+  
 }
+
+toggleButton.addEventListener('click', function (){
+    // mobileNav.style.display ='block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open')
+})
+
